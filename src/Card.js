d53dbd8f89f093e3d4ef842a1c79ec77dfc3dde6
srehan1 <syed.rehan@shoppertrak.com>
2023-01-24 11:05:41 +0530

    1- Added unique IDs for card, 2- Created a separate component CardList to have all the cards

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ import './Card.css';
 const Card = (props) => {
     const {id, name, email} = props;
     const htmlContent = 
-    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+    <div data-testid={`mabl-${id}`} className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
         <img src={`https://robohash.org/${id}?200x200`} alt='robots'></img>
         <div>
             <h2>{name}</h2>
