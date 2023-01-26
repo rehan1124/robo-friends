97f108878438859d5b0d691badc4c99d75a6f41e
srehan1 <syed.rehan@shoppertrak.com>
2023-01-26 20:38:28 +0530

    Beautification and font

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import { robots } from './robots';
 import SearchBox from "./SearchBox";
+import './App.css';
 
-const App = () => {
-    const htmlContent =
-    <div data-testid="app" className="tc">
-        <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList robots={robots}/>
-    </div>
-    return htmlContent;
+
+class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchField: ""
+        }
+    }
+
+    onSearch = (event) => {
+        this.setState({ searchField: event.target.value })
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter((items) => {
+            return items.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        });
+        const htmlContent =
+        <div data-testid="app" className="tc">
+            <h1 className="f1">RoboFriends</h1>
+            <SearchBox onSearch={ this.onSearch }/>
+            <CardList robots={filteredRobots}/>
+        </div>
+        return htmlContent;
+    }
 }
 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const SearchBox = () => {
+const SearchBox = ({ onSearch }) => {
     const htmlContent =
     <div data-testid="search-robots" className="pa2">
-        <input type="search" placeholder="Search robots ..." className="pa3 ba b--green bg-lightest-blue"></input>
+        <input type="search" placeholder="Search robots ..." className="pa3 ba b--green bg-lightest-blue" onChange={ onSearch }></input>
     </div>
     return htmlContent;
 }
