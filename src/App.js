b6849801681a9c9b3b7e9b597b16a8539db06ee8
srehan1 <syed.rehan@shoppertrak.com>
2023-01-27 18:45:27 +0530

    Added Scroll component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,52 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
-import { robots } from './robots';
 import SearchBox from "./SearchBox";
-import './App.css';
-
+import "./App.css";
+import Scroll from "./Scroll";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchField: "",
+    };
+  }
 
-    constructor() {
-        super();
-        this.state = {
-            robots: robots,
-            searchField: ""
-        }
-    }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        this.setState({ robots: users });
+      });
+  }
 
-    onSearch = (event) => {
-        this.setState({ searchField: event.target.value })
-    }
+  onSearch = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
 
-    render() {
-        const filteredRobots = this.state.robots.filter((items) => {
-            return items.name.toLowerCase().includes(this.state.searchField.toLowerCase());
-        });
-        const htmlContent =
-        <div data-testid="app" className="tc">
-            <h1 className="f1">RoboFriends</h1>
-            <SearchBox onSearch={ this.onSearch }/>
-            <CardList robots={filteredRobots}/>
-        </div>
-        return htmlContent;
+  render() {
+    const filteredRobots = this.state.robots.filter((items) => {
+      return items.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase());
+    });
+    const htmlContent = (
+      <div data-testid="app" className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox onSearch={this.onSearch} />
+        <Scroll data-testid="scroll">
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
+    if (this.state.robots.length === 0) {
+      return <h1 className="tc">Loading ...</h1>;
     }
+    return htmlContent;
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import App from './App';
-import 'tachyons';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import App from "./App";
+import "tachyons";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
